1a12ea2e6464d2e73fff63569dfe8608319649e8
FelipeDNL <felipednlled@gmail.com>
2024-03-13 11:42:29 -0300

    desisto

--- config/express.js
+++ config/express.js
@@ -1,23 +1,21 @@
-//o objetivo desse arquivo é configurar o express
-const express = require("express");
-const config = require("config");
+// Esse arquivo terá a responsabilidade de configurar o servidor express
+// O servidor será configurado no objeto app
+const express = require('express');
+const config = require('config');
+const webRoutes = require('../routes/web');
+const apiRoutes = require('../routes/api');
 const app = express();
-const webRoutes = require("../routes/web");
-const apiRoutes = require("../routes/api");
 
-//define as rotas estáticas para os arquivos da pasta public
-app.use(express.static("./public"));
-
-//guardando uma variavel chamada port dentro do objeto app
+// Guardando uma variável chamada port dentro do objeto app
 app.set("port", process.env.PORT || config.get("server.port"));
-
-//render de views utilizando o pacote hbs no express como "view engine" ou "template engine"
+// Render de views utilizando o pacote hbs no express como "view engine" ou "template engine"
 app.set("view engine", "hbs");
-
-//utilizar o arquivo routes/web.js p/ configurar as rotar do tipo WEB
+// Define as rotas estáticas para os arquivos da pasta public
+app.use(express.static("./public"));
+// middleware do Express que é usado para fazer o parsing dos dados enviados pelo cliente através de formulários HTML
+app.use(express.urlencoded({ extended: false }));
+// Defino aque irá utilizar o arquivo routes/web.js p/ configurar as rotas do tipo WEB
 app.use(webRoutes);
-//utilizar o arquivo routes/api.js p/ configurar as rotar do tipo API
+// Defino aque irá utilizar o arquivo routes/api.js p/ configurar as rotas do tipo API
 app.use(apiRoutes);
-
-// exporta o objeto app configurado
 module.exports = app;
