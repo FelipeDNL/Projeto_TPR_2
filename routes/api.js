e877d8cf94f3e253961a6d8501035fe6691bb892
FelipeDNL <Felipednlled@gmail.com>
2024-03-13 10:25:10 -0300

    test 2

--- routes/api.js
+++ routes/api.js
@@ -4,10 +4,7 @@ const DataBase = require('../config/Database')
 
 //Rotas API do Produto
 router.get("/produto", async (request, response) => {
-    const produtos = await DataBase.executeSQLQuery(`SELECT Produto.*,
-                                                        TipoProduto.descricao as TipoProduto_descricao
-                                                        FROM Produto
-                                                        JOIN TipoProduto ON Produto.TipoProduto_id = TipoProduto.id`);
+    const produtos = await DataBase.executeSQLQuery("SELECT * FROM Produto");
     response.render("Produto/index", { produtos: produtos });
 });
 
--- routes/web.js
+++ routes/web.js
@@ -8,20 +8,29 @@ router.get("/", async (request, response) => {
 });
 
 //rotas WEB do produto
-router.get("/produto", async (request, response) =>{
-    const produtos = await DataBase.executeSQLQuery("SELECT * FROM Produto");
-    response.render("Produto/index", {produtos: produtos});
+router.get("/produto", async (request, response) => {
+    const produtos = await DataBase.executeSQLQuery(`SELECT Produto.*,
+                                                        TipoProduto.descricao as TipoProduto_descricao
+                                                        FROM Produto
+                                                        JOIN TipoProduto ON Produto.TipoProduto_id = TipoProduto.id`);
+    response.render("Produto/index", { produtos: produtos });
 });
 
 //rotas API do TipoProduto
 router.get("/tipoproduto", async (request, response) => {
     const tipoProdutos = await DataBase.executeSQLQuery("SELECT * FROM TipoProduto");
-    response.render("TipoProduto/index", {tipoProdutos: tipoProdutos});
+    response.render("TipoProduto/index", { tipoProdutos: tipoProdutos });
 });
 
 //rotar WEB do recurso
 router.get("/recurso", async (request, response) => {
     response.render("Recurso/index");
+});
+
+//rota create 
+router.get("/produto/create", async (request, response) => {
+    const tipoProdutos = await DataBase.executeSQLQuery("SELECT * FROM TipoProduto");
+    response.render("Produto/create", { tipoProdutos: tipoProdutos });
 });
 
 //exporta o objeto router configurado
